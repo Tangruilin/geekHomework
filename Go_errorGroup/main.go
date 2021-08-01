171b41aad89dfe6067f32c8106b9360c2679bb77
Tangruilin <trl@hust.edu.cn>
2021-08-01 22:28:35 +0800

    第三周作业

--- Go_errorGroup/main.go
+++ Go_errorGroup/main.go
@@ -10,8 +10,16 @@ import (
 )
 
 func main() {
-	g, ctx := errgroup.WithContext(context.Background())
-
+	// 定义一个上游的ctx
+	ctx := context.Background()
+	//根据上游的ctx确定一个下游存在的ctx
+	ctx, cancel := context.WithCancel(ctx)
+	//注册errgroup
+	g, errCtx := errgroup.WithContext(ctx)
+	//初始化一个http.server
+	server := &http.Server{
+		Addr: ":8080",
+	}
 	//	起一个http server
 	g.Go(func() error {
 		http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
@@ -21,22 +29,31 @@ func main() {
 			}
 			return
 		})
-		serve := &http.Server{
-			Addr:    "8080",
-			Handler: nil,
-		}
-		err := serve.ListenAndServe()
-		defer serve.Close()
-		return err
+		//将服务监听在这里挂起
+		return server.ListenAndServe()
+	})
+	//监控协程
+	g.Go(func() error {
+		//阻塞在这里
+		<-errCtx.Done()
+		return server.Shutdown(errCtx)
 	})
 
 	g.Go(func() error {
 		c := make(chan os.Signal)
 		signal.Notify(c)
 		//通过发送信号的方式，将函数阻塞在此处
-		<-c
+		for {
+			select {
+			case <-errCtx.Done():
+				return errCtx.Err()
+			case <-c:
+				cancel()
+			}
+		}
 		return nil
 	})
+
 	err := g.Wait()
 	fmt.Println(err)
 	fmt.Println(ctx.Err())
